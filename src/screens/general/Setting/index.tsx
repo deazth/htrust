c6a96b82adbf7096deb8e8eaec7627e036c948d2
Mr.Worldwide <syah@rbtsb.com>
2022-07-15 21:54:49 +0800

    add wrapper

--- src/screens/general/Setting/index.tsx
+++ src/screens/general/Setting/index.tsx
@@ -6,6 +6,7 @@ import Constants from "expo-constants";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { setUserToken, setUserID, setUserObj } from "../../../app/userSlice";
 import { useDispatch } from "react-redux";
+import { ScreenWrapper } from "components/styles";
 import Button from "components/Button";
 import RowButton from "./RowButton";
 import { StyleSheet } from "react-native";
@@ -34,64 +35,63 @@ export function Setting({ navigation }) {
   }
 
   return (
-    <ScrollView
-      style={{ backgroundColor: "#F0F4FD" }}
-      contentContainerStyle={{ paddingVertical: 15 }}
-    >
-      {[
-        {
-          label: "About",
-          iconClass: FontAwesome5,
-          iconName: "lightbulb",
-          onPress: () => navigation.navigate("Info"),
-        },
-        {
-          label: "FAQ",
-          iconClass: FontAwesome5,
-          iconName: "question-circle",
-          onPress: () => navigation.navigate("TODO"),
-        },
-        {
-          label: "Settings",
-          iconClass: FontAwesome5,
-          iconName: "cog",
-          onPress: () => navigation.navigate("TODO"),
-        },
-      ].map((i) => (
-        <RowButton {...i} />
-      ))}
-      <View style={{ margin: 20 }} />
-      {[
-        {
-          label: "Terms of Use",
-          iconClass: FontAwesome5,
-          iconName: "sticky-note",
-          onPress: () => navigation.navigate("TODO"),
-        },
-        {
-          label: "Rate this App",
-          iconClass: FontAwesome5,
-          iconName: "star",
-          onPress: () => navigation.navigate("TODO"),
-        },
-        {
-          label: "Settings",
-          iconClass: FontAwesome5,
-          iconName: "share-alt",
-          onPress: () => navigation.navigate("TODO"),
-        },
-      ].map((i) => (
-        <RowButton {...i} />
-      ))}
-      <View style={{ padding: 20, marginTop: 40 }}>
-        <Text style={{ color: "#1C03E3", fontSize: 14, textAlign: "center" }}>
-          Version {version}
-        </Text>
-        <Text style={styles.copyright}>
-          Copyright 2022 Telekom Malaysia Berhad. All rights reserved.
-        </Text>
-        <Button label="Logout" onPress={signOut} />
-      </View>
-    </ScrollView>
+    <ScreenWrapper>
+      <ScrollView w="100%" contentContainerStyle={{ paddingVertical: 15 }}>
+        {[
+          {
+            label: "About",
+            iconClass: FontAwesome5,
+            iconName: "lightbulb",
+            onPress: () => navigation.navigate("Info"),
+          },
+          {
+            label: "FAQ",
+            iconClass: FontAwesome5,
+            iconName: "question-circle",
+            onPress: () => navigation.navigate("TODO"),
+          },
+          {
+            label: "Settings",
+            iconClass: FontAwesome5,
+            iconName: "cog",
+            onPress: () => navigation.navigate("TODO"),
+          },
+        ].map((i) => (
+          <RowButton {...i} />
+        ))}
+        <View style={{ margin: 20 }} />
+        {[
+          {
+            label: "Terms of Use",
+            iconClass: FontAwesome5,
+            iconName: "sticky-note",
+            onPress: () => navigation.navigate("TODO"),
+          },
+          {
+            label: "Rate this App",
+            iconClass: FontAwesome5,
+            iconName: "star",
+            onPress: () => navigation.navigate("TODO"),
+          },
+          {
+            label: "Settings",
+            iconClass: FontAwesome5,
+            iconName: "share-alt",
+            onPress: () => navigation.navigate("TODO"),
+          },
+        ].map((i) => (
+          <RowButton {...i} />
+        ))}
+        <View style={{ padding: 20, marginTop: 40 }}>
+          <Text style={{ color: "#1C03E3", fontSize: 14, textAlign: "center" }}>
+            Version {version}
+          </Text>
+          <Text style={styles.copyright}>
+            Copyright 2022 Telekom Malaysia Berhad. All rights reserved.
+          </Text>
+          <Button label="Logout" onPress={signOut} />
+        </View>
+      </ScrollView>
+    </ScreenWrapper>
   );
 }
