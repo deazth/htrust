35397c08389898dd4545e9994f78796332a0e8e1
Mr.Worldwide <syah@rbtsb.com>
2022-07-15 23:05:09 +0800

    fix ui about page

--- src/screens/general/About.tsx
+++ src/screens/general/About.tsx
@@ -18,6 +18,12 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 30,
   },
+  line: {
+    backgroundColor: "#C7C7C7",
+    height: 1,
+    marginVertical: 15,
+    marginHorizontal: -5,
+  },
 });
 
 const { version } = Constants.manifest;
@@ -29,6 +35,18 @@ export function About({ navigation }) {
         w="100%"
         contentContainerStyle={{ paddingVertical: 15 }}
       >
+        <View style={{ backgroundColor: "white", padding: 18 }}>
+          <Text style={{ color: "#1C03E3", fontSize: 16 }}>About</Text>
+          <View style={styles.line} />
+          <Text style={{ color: "#464646", fontSize: 14 }}>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod
+          </Text>
+        </View>
         <View style={{ padding: 20, marginTop: 40 }}>
           <Text style={{ color: "#1C03E3", fontSize: 14, textAlign: "center" }}>
             Version {version}
