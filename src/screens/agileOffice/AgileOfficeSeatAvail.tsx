e3b2edc39de777ba81568ff6adb13c1cfc096ac7
Mr.Worldwide <syah@rbtsb.com>
2022-07-17 11:46:59 +0800

    workspace reservation

--- src/screens/agileOffice/AgileOfficeSeatAvail.tsx
+++ src/screens/agileOffice/AgileOfficeSeatAvail.tsx
@@ -14,22 +14,19 @@ import {
   unifi_c9,
   unifi_primary,
 } from "components/styles";
-import MainButton from "components/Button";
+import Button from "components/Button";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import {
-  Button,
-  Box,
   CheckIcon,
   HStack,
   ScrollView,
   Select,
-  Spacer,
   Text,
   useColorModeValue,
-  Accordion,
   Icon,
-  Flex,
-  AlertDialog,
   Checkbox,
+  Modal,
+  View,
 } from "native-base";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -39,11 +36,24 @@ import {
   setUserObj,
 } from "../../app/userSlice";
 import axios from "axios";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import { Platform, StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
-import * as WebBrowser from "expo-web-browser";
-
+const dateFormat = [
+  "en-UK",
+  {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  } as Intl.DateTimeFormatOptions,
+];
+const timeFormat = [
+  "en-US",
+  {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  } as Intl.DateTimeFormatOptions,
+];
 export function AgileOfficeSeatAvail({ navigation }) {
   const dispatch = useDispatch();
 
@@ -59,12 +69,18 @@ export function AgileOfficeSeatAvail({ navigation }) {
   const [seatID, setSeatid] = React.useState(null);
   const [seatLabel, setSeatLabel] = React.useState(null);
   const [isMoreThan1Day, setIsMoreThan1Day] = React.useState(false);
-  const [showFromDPicker, setShowFromDPicker] = React.useState(false);
-  const [showToDPicker, setShowToDPicker] = React.useState(false);
-  const [showFromTPicker, setShowFromTPicker] = React.useState(false);
-  const [showToTPicker, setShowToTPicker] = React.useState(false);
-  const [fromtime, setFromtime] = React.useState(new Date());
-  const [totime, setTotime] = React.useState(new Date());
+  const [dateTimePickerConfig, setDateTimePickerConfig] = React.useState<
+    | {
+        mode: "date" | "time";
+        onChange: (event: any, date?: Date) => void;
+        value?: Date;
+      }
+    | undefined
+  >();
+  const [fromDate, setFromDate] = React.useState<Date | undefined>();
+  const [toDate, setToDate] = React.useState<Date | undefined>();
+  const [fromTime, setFromTime] = React.useState<Date | undefined>();
+  const [toTime, setToTime] = React.useState<Date | undefined>();
   const [buildList, setBuildList] = React.useState([]);
   const [floorList, setFloorList] = React.useState([]);
   const [fcList, setFcList] = React.useState([]);
@@ -76,38 +92,35 @@ export function AgileOfficeSeatAvail({ navigation }) {
 
   const [cfromtime, setCFromtime] = React.useState(new Date());
   const [ctotime, setCTotime] = React.useState(new Date());
-  const cancelRef = React.useRef(null);
-
-  // const [showLayout, setShowLayout] = React.useState(false);
-  // const [layoutUrl, setLayoutUrl] = React.useState(null);
-
-  // center spacing to and from
-  const fontFamily = Platform.OS === "ios" ? "Courier" : "monospace";
-
-  const form_bg_color = useColorModeValue(c_white, unifi_c7);
-  const acco_text_color = useColorModeValue(c_black, c_white);
-  const acco_floor_color = useColorModeValue(unifi_primary, unifi_c3);
-  const acco_fc_color = useColorModeValue(unifi_c9, unifi_c2);
   const placeholderTextColor = useColorModeValue("#C2C2C2", unifi_c9);
-  const btn_bg_color = useColorModeValue(unifi_c4, unifi_primary);
+  const textColor = useColorModeValue("black", unifi_c9);
+  const selectBorderColor = useColorModeValue("#707070", undefined);
+  const selectBackgroundColor = useColorModeValue("white", "#707070");
 
   const selectStyle = {
     placeholderTextColor,
-    borderColor: "#707070",
-    backgroundColor: "white",
+    borderColor: selectBorderColor,
+    backgroundColor: selectBackgroundColor,
   };
 
-  function showFloorLayout(floor_id) {
-    // setLayoutUrl(baseurl + 'ao/getFloorLayout?id=' + floor_id);
-    WebBrowser.openBrowserAsync(baseurl + "ao/getFloorLayout?id=" + floor_id);
-    // setShowLayout(true);
-  }
+  const dateTimeSelectStyle = (type: string): {} => ({
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: type === "full" ? "100%" : "50%",
+    alignItems: "center",
+    backgroundColor: selectBackgroundColor,
+    padding: 10,
+    borderColor: selectBorderColor,
+    borderWidth: 1,
+    borderLeftWidth: type === "right" ? 0 : undefined,
+    borderTopLeftRadius: type === "right" ? 0 : 6,
+    borderBottomLeftRadius: type === "right" ? 0 : 6,
+    borderTopRightRadius: type === "left" ? 0 : 6,
+    borderBottomRightRadius: type === "left" ? 0 : 6,
+  });
 
-  function showSectionLayout(fcc_id) {
-    // setLayoutUrl(baseurl + 'ao/getSectionLayout?id=' + fcc_id);
-    WebBrowser.openBrowserAsync(baseurl + "ao/getSectionLayout?id=" + fcc_id);
-    // setShowLayout(true);
-  }
+  const placeholderStyle = { fontSize: 16, color: placeholderTextColor };
+  const textStyle = { fontSize: 16, color: textColor };
 
   function selectBuilding(building_id) {
     setIsloading(true);
@@ -143,8 +156,8 @@ export function AgileOfficeSeatAvail({ navigation }) {
             // alert(JSON.stringify(response.data.data));
             const resptime = new Date(response.headers.date);
             const futtime = new Date(resptime.getTime() + 5 * 60000);
-            setFromtime(futtime);
-            setTotime(futtime);
+            setFromTime(futtime);
+            setToTime(futtime);
             setBuildList(response.data.data);
           } else {
             alert("Failed to fetch building list from the server");
@@ -253,13 +266,13 @@ export function AgileOfficeSeatAvail({ navigation }) {
 
   function searchAvail() {
     setIsloading(true);
-    setCFromtime(fromtime);
-    setCTotime(totime);
+    setCFromtime(fromTime);
+    setCTotime(toTime);
     setSearchPressed(false);
 
-    const offsetMs = fromtime.getTimezoneOffset() * 60 * 1000;
-    const dateLocalF = new Date(fromtime.getTime() - offsetMs);
-    const dateLocalT = new Date(totime.getTime() - offsetMs);
+    const offsetMs = fromTime.getTimezoneOffset() * 60 * 1000;
+    const dateLocalF = new Date(fromTime.getTime() - offsetMs);
+    const dateLocalT = new Date(toTime.getTime() - offsetMs);
 
     const inputs = {
       building_id: bID,
@@ -373,34 +386,6 @@ export function AgileOfficeSeatAvail({ navigation }) {
     setShowConfirm(false);
   }
 
-  const showChangeFromDate = () => setShowFromDPicker(true);
-
-  const showChangeToDate = () => setShowToDPicker(true);
-
-  const showChangeFromTime = () => setShowFromTPicker(true);
-
-  const showChangeToTime = () => setShowToTPicker(true);
-
-  const changeFromDate = (event, selectedDate) => {
-    setShowFromDPicker(false);
-    setFromtime(selectedDate || fromtime);
-  };
-
-  const changeFromTime = (event, selectedDate) => {
-    setShowFromTPicker(false);
-    setFromtime(selectedDate || fromtime);
-  };
-
-  const changeToDate = (event, selectedDate) => {
-    setShowToDPicker(false);
-    setTotime(selectedDate || fromtime);
-  };
-
-  const changeToTime = (event, selectedDate) => {
-    setShowToTPicker(false);
-    setTotime(selectedDate || fromtime);
-  };
-
   React.useEffect(() => {
     setIsloading(true);
     loadBuildList();
@@ -412,7 +397,7 @@ export function AgileOfficeSeatAvail({ navigation }) {
   return (
     <ScreenWrapper>
       <ScrollView w="100%" h="100%" contentContainerStyle={{ padding: 20 }}>
-        <Text style={{ color, fontSize: 14 }}>
+        <Text style={{ color, fontSize: 14, fontWeight: "bold" }}>
           Search for available workspace
         </Text>
         <Text style={styles.label}>Choose Building</Text>
@@ -507,222 +492,107 @@ export function AgileOfficeSeatAvail({ navigation }) {
           </Checkbox.Group>
           <Text style={{ fontSize: 14 }}>More Than One Day</Text>
         </HStack>
-
         <HStack my={3} alignItems="flex-start">
-          <Text fontSize="sm" style={{ fontFamily }}>
-            From :
-          </Text>
-          <Spacer />
-          <Text
-            fontSize="sm"
-            color={placeholderTextColor}
-            onPress={showChangeFromDate}
+          <Pressable
+            style={dateTimeSelectStyle(isMoreThan1Day ? "left" : "full")}
+            onPress={() =>
+              setDateTimePickerConfig({
+                mode: "date",
+                onChange: (_, d) => setFromDate(d),
+                value: fromDate || new Date(),
+              })
+            }
           >
-            {fromtime.toLocaleDateString()}
-          </Text>
-          <Spacer />
-          <Text
-            fontSize="sm"
-            color={placeholderTextColor}
-            onPress={showChangeFromTime}
-          >
-            {fromtime.toLocaleTimeString()}
-          </Text>
+            <Text style={fromDate ? textStyle : placeholderStyle}>
+              {fromDate
+                ? fromDate.toLocaleDateString(...dateFormat)
+                : isMoreThan1Day
+                ? "Start Date"
+                : "Please Select"}
+            </Text>
+            <Icon as={FontAwesome5} name="chevron-down" size={4} />
+          </Pressable>
+          {isMoreThan1Day && (
+            <Pressable
+              style={dateTimeSelectStyle("right")}
+              onPress={() =>
+                setDateTimePickerConfig({
+                  mode: "date",
+                  onChange: (_, d) => setToDate(d),
+                  value: toDate || new Date(),
+                })
+              }
+            >
+              <Text style={toDate ? textStyle : placeholderStyle}>
+                {toDate ? toDate.toLocaleDateString(...dateFormat) : "End Date"}
+              </Text>
+              <Icon as={FontAwesome5} name="chevron-down" size={4} />
+            </Pressable>
+          )}
         </HStack>
-
-        <HStack mb={3} alignItems="flex-start">
-          <Text fontSize="sm" style={{ fontFamily }}>
-            To :
-          </Text>
-          <Spacer />
-          <Text
-            fontSize="sm"
-            color={placeholderTextColor}
-            onPress={showChangeToDate}
+        <Text style={styles.label}>Choose Time</Text>
+        <HStack my={3} alignItems="flex-start">
+          <Pressable
+            style={dateTimeSelectStyle("left")}
+            onPress={() =>
+              setDateTimePickerConfig({
+                mode: "time",
+                onChange: (_, d) => setFromTime(d),
+                value: fromTime || new Date(),
+              })
+            }
           >
-            {totime.toLocaleDateString()}
-          </Text>
-          <Spacer />
-          <Text
-            fontSize="sm"
-            color={placeholderTextColor}
-            onPress={showChangeToTime}
+            <Text style={fromTime ? textStyle : placeholderStyle}>
+              {fromTime
+                ? fromTime.toLocaleTimeString(...timeFormat)
+                : "Start Time"}
+            </Text>
+            <Icon as={FontAwesome5} name="chevron-down" size={4} />
+          </Pressable>
+          <Pressable
+            style={dateTimeSelectStyle("right")}
+            onPress={() =>
+              setDateTimePickerConfig({
+                mode: "time",
+                onChange: (_, d) => setToTime(d),
+                value: toTime || new Date(),
+              })
+            }
           >
-            {totime.toLocaleTimeString()}
-          </Text>
+            <Text style={toTime ? textStyle : placeholderStyle}>
+              {toTime ? toTime.toLocaleTimeString(...timeFormat) : "End Time"}
+            </Text>
+            <Icon as={FontAwesome5} name="chevron-down" size={4} />
+          </Pressable>
         </HStack>
-
-        <MainButton
+        <Button
+          style={{ marginTop: 20 }}
           label={isLoading ? "Please wait" : "Search"}
           onPress={searchAvail}
           loading={isLoading}
         />
-
-        {showFromDPicker && (
-          <DateTimePicker
-            testID="showFromDPicker"
-            value={fromtime}
-            mode="date"
-            is24Hour
-            display="default"
-            onChange={changeFromDate}
-          />
-        )}
-        {showFromTPicker && (
-          <DateTimePicker
-            testID="showFromTPicker"
-            value={fromtime}
-            mode="time"
-            is24Hour
-            display="default"
-            onChange={changeFromTime}
-          />
-        )}
-        {showToDPicker && (
-          <DateTimePicker
-            testID="showToDPicker"
-            value={totime}
-            mode="date"
-            is24Hour
-            display="default"
-            onChange={changeToDate}
-          />
-        )}
-        {showToTPicker && (
-          <DateTimePicker
-            testID="showToTPicker"
-            value={totime}
-            mode="time"
-            is24Hour
-            display="default"
-            onChange={changeToTime}
-          />
-        )}
-
-        {searchPressed && (
-          <Box border={1} m={3} p={2} bg={form_bg_color} w="95%" rounded="10px">
-            {sResult.length > 0 ? (
-              <>
-                <Text>Search Result</Text>
-                <Accordion>
-                  {sResult.map((rec) => (
-                    <Accordion.Item key={rec.id}>
-                      <Accordion.Summary _expanded={{ bg: acco_floor_color }}>
-                        <Text color={acco_text_color}>
-                          {rec.name} - {rec.count} seats
-                        </Text>
-                        <Accordion.Icon />
-                      </Accordion.Summary>
-                      <Accordion.Details>
-                        {rec.gotlayout && (
-                          <Button onPress={() => showFloorLayout(rec.id)}>
-                            View Floor Layout
-                          </Button>
-                        )}
-                        <Accordion>
-                          {rec.fcs.map((fc) => {
-                            return (
-                              <Accordion.Item key={fc.id}>
-                                <Accordion.Summary
-                                  _expanded={{ bg: acco_fc_color }}
-                                >
-                                  <Text color={acco_text_color}>
-                                    {fc.name} - {fc.count} seats
-                                  </Text>
-                                  <Accordion.Icon />
-                                </Accordion.Summary>
-                                <Accordion.Details>
-                                  {fc.gotlayout && (
-                                    <Button
-                                      onPress={() => showSectionLayout(fc.id)}
-                                    >
-                                      View Section Layout
-                                    </Button>
-                                  )}
-                                  <Flex
-                                    direction="row"
-                                    flexWrap="wrap"
-                                    justifyContent="space-evenly"
-                                  >
-                                    {fc.seats.map((seat) => {
-                                      return (
-                                        <Button
-                                          m={1}
-                                          p={1}
-                                          backgroundColor={btn_bg_color}
-                                          key={seat.id}
-                                          endIcon={
-                                            <Icon
-                                              as={FontAwesome5}
-                                              name="calendar-check"
-                                              size={4}
-                                            />
-                                          }
-                                          onPress={() =>
-                                            selectSeat(seat.id, seat.name)
-                                          }
-                                        >
-                                          {seat.name}
-                                        </Button>
-                                      );
-                                    })}
-                                  </Flex>
-                                </Accordion.Details>
-                              </Accordion.Item>
-                            );
-                          })}
-                        </Accordion>
-                      </Accordion.Details>
-                    </Accordion.Item>
-                  ))}
-                </Accordion>
-              </>
-            ) : (
-              <Text>No seat available for the selected parameter</Text>
-            )}
-
-            <AlertDialog
-              leastDestructiveRef={cancelRef}
-              isOpen={showConfirm}
-              onClose={() => setShowConfirm(false)}
-            >
-              <AlertDialog.Content>
-                <AlertDialog.CloseButton
-                  icon={<Icon as={FontAwesome5} name="times" size={4} />}
-                />
-                <AlertDialog.Header>{seatLabel}</AlertDialog.Header>
-                <AlertDialog.Body>
-                  <Text>Confirm reserve this workspace?</Text>
-                  <Text>From: {cfromtime.toLocaleString()}</Text>
-                  <Text>To: {ctotime.toLocaleString()}</Text>
-                </AlertDialog.Body>
-                <AlertDialog.Footer>
-                  <Button.Group space={2}>
-                    <Button
-                      variant="unstyled"
-                      colorScheme="coolGray"
-                      onPress={() => {
-                        setShowConfirm(false);
-                      }}
-                      ref={cancelRef}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      isLoading={isLoading}
-                      isLoadingText="Please Wait"
-                      colorScheme="primary"
-                      onPress={() => doSeatReserve()}
-                    >
-                      Reserve
-                    </Button>
-                  </Button.Group>
-                </AlertDialog.Footer>
-              </AlertDialog.Content>
-            </AlertDialog>
-          </Box>
-        )}
       </ScrollView>
+      <Modal
+        isOpen={!!dateTimePickerConfig}
+        onClose={() => setDateTimePickerConfig(undefined)}
+      >
+        <Modal.Content w="100%" marginBottom={0} marginTop="auto">
+          {/* <Modal.CloseButton /> */}
+          {/* <Modal.Header>Select</Modal.Header> */}
+          <Modal.Body>
+            {!!dateTimePickerConfig && (
+              <DateTimePicker
+                value={dateTimePickerConfig.value}
+                mode={dateTimePickerConfig.mode}
+                display="spinner"
+                is24Hour
+                onChange={dateTimePickerConfig.onChange}
+              />
+            )}
+            <View style={{ height: 10 }} />
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
     </ScreenWrapper>
   );
 }
