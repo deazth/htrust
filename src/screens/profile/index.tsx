2ac8a17a979447103d37a831a14c4e1c2a7ae964
Mr.Worldwide <syah@rbtsb.com>
2022-07-26 08:20:17 +0800

    add subordinates

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { View, Button, Text, useColorModeValue } from "native-base";
+import {
+  View,
+  Button,
+  Text,
+  useColorModeValue,
+  Icon,
+  VStack,
+} from "native-base";
 
 import { ScreenWrapper, unifi_c4, unifi_c1 } from "components/styles";
 import { StyleSheet } from "react-native";
@@ -13,21 +20,46 @@ export const Profile = ({ navigation }) => {
     <ScreenWrapper>
       <View style={{ ...styles.box, backgroundColor }}>
         <View style={{ flexDirection: "row" }}>
-          <View
-            style={{
-              borderColor: "white",
-              borderWidth: 3,
-              backgroundColor: "gray",
-              height: 80,
-              width: 80,
-              borderRadius: "50%",
-            }}
-          />
+          <View style={styles.profileImage} />
           <Text style={{ color: highlightColor, flex: 5 }}>
             Telekom Malaysia Berhad
           </Text>
         </View>
       </View>
+      <VStack space={2} style={{ ...styles.box, backgroundColor }}>
+        <View style={styles.row}>
+          <Text style={{ color: highlightColor }}>Skillsets</Text>
+          <Icon
+            color={highlightColor}
+            as={FontAwesome5}
+            name="plus-circle"
+            size={4}
+          />
+        </View>
+        <View style={{ width: "100%", height: 1, backgroundColor: "#ddd" }} />
+        <View style={styles.row}>
+          <Text>Skillsets</Text>
+          <Icon color={highlightColor} as={FontAwesome5} name="edit" size={4} />
+        </View>
+      </VStack>
+      <VStack space={2} style={{ ...styles.box, backgroundColor }}>
+        <View style={styles.row}>
+          <Text style={{ color: highlightColor }}>Direct Supervisor</Text>
+        </View>
+        <View style={{ width: "100%", height: 1, backgroundColor: "#ddd" }} />
+      </VStack>
+      <VStack space={2} style={{ ...styles.box, backgroundColor }}>
+        <View style={styles.row}>
+          <Text style={{ color: highlightColor }}>Subordinate</Text>
+          <Icon
+            color={highlightColor}
+            as={FontAwesome5}
+            name="chevron-circle-right"
+            size={4}
+          />
+        </View>
+        <View style={{ width: "100%", height: 1, backgroundColor: "#ddd" }} />
+      </VStack>
     </ScreenWrapper>
   );
 };
@@ -46,5 +78,18 @@ const styles = StyleSheet.create({
     elevation: 8,
     paddingVertical: 15,
     paddingHorizontal: 20,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  profileImage: {
+    borderColor: "white",
+    borderWidth: 3,
+    backgroundColor: "gray",
+    height: 80,
+    width: 80,
+    borderRadius: "50%",
   },
 });
